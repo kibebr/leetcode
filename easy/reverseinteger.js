1d0f8d7787c6c5727abeaa7221e4e23503c39472
kibe <0763722@students.dadeschools.net>
2020-05-20 16:07:09 -0400

    added first leetcode problem

---
+++ easy/reverseinteger.js
@@ -0,0 +1,22 @@
+var reverse = function(x)
+{
+	const isNeg = x < 0 
+	            ? true
+	            : false;
+
+	if(isNeg)
+		x = x * -1;
+
+	let reversed = 0;
+
+	while(x > 0){
+		reversed = (reversed * 10) + (x%10);
+		x = parseInt(x/10);
+	}
+
+	return (isNeg)
+		   ? reversed * -1
+		   : reversed;
+};
+
+console.log(reverse(123));

d2f62b46b2966ba0c1ef5c116627080b829dace4
kibe <0763722@students.dadeschools.net>
2020-06-14 20:57:29 -0400

    added 'First Unique Character in a String' exercise

---
+++ easy/uniquechar.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const firstUniqChar = function (s) {
+  const map = new Map()
+
+  for (let i = 0; s[i]; ++i) {
+    if (map.has(s[i])) {
+      map.set(s[i], { occurrences: map.get(s[i]).occurrences+1, index: map.get(s[i]).index })
+    } else {
+      map.set(s[i], { occurrences: 1, index: i })
+    }
+  }
+
+  for (const [key, val] of map) {
+    if (val.occurrences === 1) {
+      return val.index
+    }
+  }
+
+  return -1
+}
